f1f75e70be0b6c9659e5ef998d8adc906583a8cf
Daniel Agar <daniel@agar.ca>
2021-08-21 16:23:41 -0400

    WIP

---
+++ boards/px4/fmu-v3/nuttx-config/include/nsh_romfsimg.h
@@ -0,0 +1,9 @@
+/**
+ * nsh_romfsimg.h
+ *
+ * This file is a stub for 'make export' purposes; the actual ROMFS
+ * must be supplied by the library client.
+ */
+
+extern unsigned char romfs_img[];
+extern unsigned int romfs_img_len;
